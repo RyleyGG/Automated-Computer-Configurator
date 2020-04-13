281ffba89d20c0c79ee51bad490b90755f71e7f8
RyleyGG <griffithryley2000@gmail.com>
2020-04-13 16:11:39 -0400

    Added application requirement
    It is now required of the user to enter at least 1 application before proceeding further.

--- src/app/GUIController.java
+++ src/app/GUIController.java
@@ -48,67 +48,80 @@ public class GUIController extends Application
                 String alertString = "";
                 GenericApplication[] appList = configurator.getAppList().toArray(new GenericApplication[0]);
     
-                for (int i = 0; i < appList.length; i++)
+                if (appList.length > 0)
                 {
-                    alertString += appList[i].getName() + "\n";
-                }
-    
-                Alert confirmationAlert = new Alert(AlertType.CONFIRMATION);
-                confirmationAlert.setTitle("Application Submission");
-                confirmationAlert.setHeaderText("Confirm that these are the applications you wish to submit.");
-                ((Button) confirmationAlert.getDialogPane().lookupButton(ButtonType.OK)).setText("Yes");
-                ((Button) confirmationAlert.getDialogPane().lookupButton(ButtonType.CANCEL)).setText("No");
-                confirmationAlert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE); //Ensures alert window always fits the full text
-                confirmationAlert.setContentText(alertString);
-                confirmationAlert.showAndWait();  
-                
-                if (confirmationAlert.getResult() == ButtonType.OK)
-                {
-                    UserPreferencesGUI userPreferencesGUI = new UserPreferencesGUI(scene, configurator);
-                    rootPane.getChildren().set(0,userPreferencesGUI);
-                    
-                    
-                    userPreferencesGUI.lookup("#userPreferencesSubmitButton").setOnMouseReleased(g ->
+                    for (int i = 0; i < appList.length; i++)
                     {
-                        int userBudget = userPreferencesGUI.getBudget();
-                        int monitorCount = userPreferencesGUI.getMonitorCount();
-                        String monitorRes = userPreferencesGUI.getMonitorRes();
-                        List<String> selectedConfigs = userPreferencesGUI.getSelectedConfigs();
-                        String[] selectedConfigsArr = selectedConfigs.toArray(new String[0]);
-                        String configString = "";
-
-                        if (selectedConfigsArr.length > 0 && monitorRes != null && monitorCount > -1 && userBudget > -1)
-                        {
-                            for (int i = 0; i < selectedConfigsArr.length; i++)
-                            {
-                                configString += selectedConfigsArr[i]+"\n";
-                            }
-
-                            confirmationAlert.setTitle("Preference Confirmation");
-                            confirmationAlert.setHeaderText("Please confirm that the preferences below are correct");
-                            confirmationAlert.setContentText("Budget: " + userBudget + "\nMonitor Count: " + monitorCount + "\nPreferred Monitor Resolution: " + monitorRes + "\n\nSelected Configuration(s):\n" + configString);
-                            confirmationAlert.showAndWait();
-
-                            if (confirmationAlert.getResult() == ButtonType.OK)
-                            {
-                                configurator.setUserBudget(userBudget);
-                                configurator.setMonitorCount(monitorCount);
-                                configurator.setMonitorRes(monitorRes);
-                                configurator.setSelectedConfigs(selectedConfigs);
-                                System.out.println("Proceeding onto product GUI");
-                            }
-                        }
-                        else
-                        {
-                            Alert configAlert = new Alert(AlertType.INFORMATION);
-                            configAlert.setTitle("Action needed");
-                            configAlert.setHeaderText("Warning: not all fields completed");
-                            configAlert.getDialogPane().setContentText("At least one of the fields haven't been completed. Make sure that each field has a value before proceeding.");
-                            configAlert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE); //Ensures alert window always fits the full text
-                            configAlert.showAndWait();
-                        }
-                    });
+                        alertString += appList[i].getName() + "\n";
+                    }
+        
+                    Alert confirmationAlert = new Alert(AlertType.CONFIRMATION);
+                    confirmationAlert.setTitle("Application Submission");
+                    confirmationAlert.setHeaderText("Confirm that these are the applications you wish to submit.");
+                    ((Button) confirmationAlert.getDialogPane().lookupButton(ButtonType.OK)).setText("Yes");
+                    ((Button) confirmationAlert.getDialogPane().lookupButton(ButtonType.CANCEL)).setText("No");
+                    confirmationAlert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE); //Ensures alert window always fits the full text
+                    confirmationAlert.setContentText(alertString);
+                    confirmationAlert.showAndWait();  
                     
+                    if (confirmationAlert.getResult() == ButtonType.OK)
+                    {
+                        UserPreferencesGUI userPreferencesGUI = new UserPreferencesGUI(scene, configurator);
+                        rootPane.getChildren().set(0,userPreferencesGUI);
+                        
+                        
+                        userPreferencesGUI.lookup("#userPreferencesSubmitButton").setOnMouseReleased(g ->
+                        {
+                            int userBudget = userPreferencesGUI.getBudget();
+                            int monitorCount = userPreferencesGUI.getMonitorCount();
+                            String monitorRes = userPreferencesGUI.getMonitorRes();
+                            List<String> selectedConfigs = userPreferencesGUI.getSelectedConfigs();
+                            String[] selectedConfigsArr = selectedConfigs.toArray(new String[0]);
+                            String configString = "";
+
+                            if (selectedConfigsArr.length > 0 && monitorRes != null && monitorCount > -1 && userBudget > -1)
+                            {
+                                for (int i = 0; i < selectedConfigsArr.length; i++)
+                                {
+                                    configString += selectedConfigsArr[i]+"\n";
+                                }
+
+                                confirmationAlert.setTitle("Preference Confirmation");
+                                confirmationAlert.setHeaderText("Please confirm that the preferences below are correct");
+                                confirmationAlert.setContentText("Budget: " + userBudget + "\nMonitor Count: " + monitorCount + "\nPreferred Monitor Resolution: " + monitorRes + "\n\nSelected Configuration(s):\n" + configString);
+                                confirmationAlert.showAndWait();
+
+                                if (confirmationAlert.getResult() == ButtonType.OK)
+                                {
+                                    configurator.setUserBudget(userBudget);
+                                    configurator.setMonitorCount(monitorCount);
+                                    configurator.setMonitorRes(monitorRes);
+                                    configurator.setSelectedConfigs(selectedConfigs);
+                                    System.out.println("Proceeding onto product GUI");
+                                }
+                            }
+                            else
+                            {
+                                Alert configAlert = new Alert(AlertType.INFORMATION);
+                                configAlert.setTitle("Action needed");
+                                configAlert.setHeaderText("Warning: not all fields completed");
+                                configAlert.getDialogPane().setContentText("At least one of the fields haven't been completed. Make sure that each field has a value before proceeding.");
+                                configAlert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE); //Ensures alert window always fits the full text
+                                configAlert.showAndWait();
+                            }
+                        });
+                        
+                    }
+                }
+
+                else
+                {
+                    Alert infoAlert = new Alert(AlertType.INFORMATION);
+                    infoAlert.setTitle("Cannot proceed");
+                    infoAlert.setHeaderText("Warning: no applications submitted");
+                    infoAlert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE); //Ensures alert window always fits the full text
+                    infoAlert.setContentText("The program cannot proceed until you have entered at least one suitable application. Remember that the more programs you enter, the more likely it is that the generated build will suit all of your needs.");
+                    infoAlert.showAndWait();  
                 }
             });
         });
